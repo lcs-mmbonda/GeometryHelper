4b1fcd75dec8b1c078fee04a2f37da112e9d764b
Myers <mmbonda@lcs.on.ca>
2024-01-16 14:45:04 -0500

    Initial Commit

---
+++ GeometryHelper/GeometryHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryHelperApp.swift
+//  GeometryHelper
+//
+//  Created by Myers Elliott Mbonda on 2024-01-16.
+//
+
+import SwiftUI
+
+@main
+struct GeometryHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
